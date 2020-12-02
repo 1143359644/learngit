8dbdbbc33aede297fa37381e3f3ace0c79495d09
huangdaqi <1143359644@qq.com>
2020-12-02 18:12:54 +0800

    SumThread

---
+++ demo2/Sum.java
@@ -0,0 +1,27 @@
+package demo2;
+
+public class Sum {
+    private long ans;
+    private int target;
+
+    public Sum(long ans, int target) {
+        this.ans = ans;
+        this.target = target;
+    }
+
+    public long getAns() {
+        return ans;
+    }
+
+    public void setAns(long ans) {
+        this.ans = ans;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public void setTarget(int target) {
+        this.target = target;
+    }
+}
